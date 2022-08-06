9da38e616bcc6ca0107fd68bfd064dc93a3fe1b4
Steve <asapcrafter@gmail.com>
2022-08-06 00:00:54 -0700

    started url module

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const http = require('http')
+const fs = require('fs')
 
 const port = process.env.POR || 8080
 
 const server = http.createServer((req, res) => {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html')
-        res.end('hello world')
+    let fileName = req.url
+    if (req.url === '/') fileName = './index'
+        fs.readFile(`${fileName}.html`, function(err, data){
+            if (err) {
+                res.writeHead('.404.html', {'Content-Type': 'text.html'})
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end
+        })
+        // res.statusCode = 200
+        // res.setHeader('Content-Type', 'text/html')
+        // res.end('hello world')
     }
 );
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port}. View the page with this url "localhost:${port}"`)
 })
