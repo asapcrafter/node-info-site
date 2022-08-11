e010908b31316e37210188a047eb5f7a316b9ff3
Steve <asapcrafter@gmail.com>
2022-08-11 00:08:05 -0700

    finished working url request handler

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const http = require('http')
 const fs = require('fs')
 
 const port = process.env.POR || 8080
-
 const server = http.createServer((req, res) => {
     let fileName = req.url
-    if (req.url === '/') fileName = './index'
-        fs.readFile(`${fileName}.html`, function(err, data){
-            if (err) {
-                res.writeHead('.404.html', {'Content-Type': 'text.html'})
-            }
+    if (req.url === '/') fileName = '/index'
+    
+    fs.readFile(`.${fileName}.html`, function(err, data){
+        if (err) {
+            fs.readFile('./404.html', (err, content) => {
+                res.writeHead(404, {'Content-Type': 'text.html'})
+                console.log('Error')
+                return res.end(content)
+            })
+        } else {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
-            return res.end
-        })
-        // res.statusCode = 200
-        // res.setHeader('Content-Type', 'text/html')
-        // res.end('hello world')
-    }
-);
+            return res.end()
+        }
+    })   
+});
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}. View the page with this url "localhost:${port}"`)
+    console.log(`Server is running on port . View the page with this url "localhost:8080"`)
 })
